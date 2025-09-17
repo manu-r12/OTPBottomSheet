6e2498a222f726b55dd1eaa22f823be07eb6eb8f
Manu R <manurajbhar12@gmail.com>
2025-09-17 22:55:29 +0530

    Initial Commit

---
+++ OTPBottomSheet/OTPBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPBottomSheetApp.swift
+//  OTPBottomSheet
+//
+//  Created by Manu on 2025-09-17.
+//
+
+import SwiftUI
+
+@main
+struct OTPBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
